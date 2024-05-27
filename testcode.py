6594170cfe1592e374dd2474338d3b0fa07ed0ee
majdhilal <majd.hilal@outlook.com>
2024-05-27 13:12:27 +0300

    first commit

---
+++ testcode.py
@@ -0,0 +1,16 @@
+array1 = ["Alice", "Bob", "Charlie", "Frank", "Jack", "Jack"]
+array2 = ["David", "Eve", "Charlie", "Alice", "Bob", "Frank", "Grace", "Liam"]
+array3 = ["Henry", "Ivy", "Jack", "Kate", "Alice", "Liam", "Ivy"]
+array4 = ["David", "Ivy", "Eve", "Bob", "Frank", "Liam", "Frank", "Charlie", "Alice"]
+
+arr = array1 + array2 + array3 + array4
+dictionary = {}
+
+for name in arr:
+    if name in dictionary:
+        dictionary[name] += 1
+    else:
+        dictionary[name] = 1
+
+for name, count in dictionary.items():
+    print(f"{name}: {count}")
